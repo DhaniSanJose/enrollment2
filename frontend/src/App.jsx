e5e86665cfbf774368315127dfc436f389a4d8a9
DhaniSanJose <furrdoy@gmail.com>
2025-04-29 20:02:10 +0800

    8th commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,13 @@ import CourseTagging from "./components/CourseTagging";
 import UserRegistrationForm from "./components/UserRegistrationForm";
 import UserProfileViewer from "./components/UserProfileViewer";
 import StudentNumbering from "./components/StudentNumbering";
+import COR from "./components/COR";
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<UserRegistrationForm />} />
+        <Route path="/cor" element={<COR />} />
         <Route path="/user" element={<UserProfileViewer />} />
         <Route path="/student-numbering" element={<StudentNumbering />} />
         <Route path="/course-tagging" element={<CourseTagging />} />
